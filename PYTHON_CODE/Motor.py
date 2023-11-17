d47ee92abcad8764c5e45304cdaf2c46bf8562de
LaurenceLamarche <laurence.lamarchecliche@gmail.com>
2023-11-17 14:23:13 -0500

    refactored into OO design

---
+++ PYTHON_CODE/Motor.py
@@ -0,0 +1,52 @@
+import machine
+import time
+
+class Motor:
+    def __init__(self):
+        # Define the pins connected to the A4988 driver
+        self.step_pin = machine.Pin(3, machine.Pin.OUT)
+        self.dir_pin = machine.Pin(2, machine.Pin.OUT)
+        self.enable_pin = machine.Pin(14, machine.Pin.OUT)
+        self.MS1_pin = machine.Pin(7, machine.Pin.OUT)
+        self.MS2_pin = machine.Pin(6, machine.Pin.OUT)
+        self.MS3_pin = machine.Pin(5, machine.Pin.OUT)
+
+        # Set initial motor state
+        self.enable_motor(False)
+        self.set_step_size(False, False)
+        self.set_direction(True)
+
+        # Define the number of steps for a full revolution (may vary with your motor)
+        self.steps_per_rev = 20000
+
+        # Calculate the delay for a 2-second half revolution
+        self.total_time = 120  # Total time for half revolution in seconds
+
+    def enable_motor(self, enable):
+        # Enable or disable the motor
+        self.enable_pin.value(enable)
+
+    def set_step_size(self, MS1, MS2):
+        # Set step size
+        self.MS1_pin.value(MS1)
+        self.MS2_pin.value(MS2)
+
+    def set_direction(self, direction):
+        # Set the direction
+        self.dir_pin.value(direction)
+
+    def move(self, num_steps, total_time):
+        # Calculate the delay based on the total time and the number of steps
+        delay = total_time / num_steps
+        self.enable_motor(True)
+
+        print("Performing steps...")
+        for _ in range(num_steps):
+            self.step_pin.on()
+            time.sleep(delay / 2)  # Half the delay for the step on time
+            self.step_pin.off()
+            time.sleep(delay / 2)  # Half the delay for the step off time
+
+        # Ensure that the stepper motor is stopped when done
+        self.step_pin.off()
+        self.enable_motor(False)
--- PYTHON_CODE/main.py
+++ PYTHON_CODE/main.py
@@ -1,41 +1,58 @@
 import machine
 import time
 import uos
+from Motor import Motor
 
-# Function to combine sensor readings into a single data packet
-def combine_sensor_readings():
-    # Read sensor values
-    # TODO: replace with actual real function from DataCollection.py
-    # combined_data = f"{photodetector_value},{motor_angle},{temperature_1},{temperature_2}"
-    
-    return "65535,32768,1023,512"
+class PayloadFlightControl:
 
+    def __init__(self):
+        # Initialize Motor instance
+        self.motor = Motor()
+        # Set up any other initializations if necessary
+
+    # Function to combine sensor readings into a single data packet
+    def combine_sensor_readings(self):
+        # Read sensor values
+        # TODO: replace with actual real function from DataCollection.py
+        # combined_data = f"{photodetector_value},{motor_angle},{temperature_1},{temperature_2}"
+        
+        return "65535,32768,1023,512"
+
+    def start_collection(self):
+        # TODO: this loop should start data collection. 
+            # Open (or create) a file to store the data
+        with open('sensor_data.txt', 'w') as file:
+            for loop_number in range(1, 4):  # Outer loop, runs three times
+                for step_count in range(4000):  # Inner loop, runs 4000 times for each outer loop
+                    # Get combined sensor reading
+                    combined_data = self.combine_sensor_readings()
+
+                    # Append the loop number to the data
+                    data_to_write = f"{loop_number},{combined_data}\n"
+
+                    # Write the data to the file
+                    file.write(data_to_write)
+
+                    # Change motor angle here
+                    # Assuming 'perform_steps' changes the motor angle and 'num_steps' is the number of steps for desired angle change
+
+                    # TODO: the motor needs to move here after we collected the data
+                    self.motor.move(1000, 120)  # Move 1000 steps in 120 seconds
+                    
+                # Optional: a small delay between each outer loop iteration
+                time.sleep(1)
+                # TODO: call the calibration algorithm HERE
+            
+        print("One full sample complete for all LEDs. Data stored in 'sensor_data.txt'.")
 
 def main():
-    # Open (or create) a file to store the data
-    with open('sensor_data.txt', 'w') as file:
-        for loop_number in range(1, 4):  # Outer loop, runs three times
-            for step_count in range(4000):  # Inner loop, runs 4000 times for each outer loop
-                # Get combined sensor reading
-                combined_data = combine_sensor_readings()
+    # TODO: this loop should listen for ground commands, and either 
+    # START DATA COLLECTION
+    # STOP DATA COLLECTION
+    # FORCE CALIBRATE
+    # GET DATA
 
-                # Append the loop number to the data
-                data_to_write = f"{loop_number},{combined_data}\n"
-
-                # Write the data to the file
-                file.write(data_to_write)
-
-                # Change motor angle here
-                # Assuming 'perform_steps' changes the motor angle and 'num_steps' is the number of steps for desired angle change
-                perform_steps(num_steps)
-                # TODO: the motor needs to move here after we collected the data
-                
-            # Optional: a small delay between each outer loop iteration
-            time.sleep(1)
-            # TODO: call the calibration algorithm HERE
-        
-
-    print("One full sample complete for all LEDs. Data stored in 'sensor_data.txt'.")
+    print("This is the main loop")
 
 # Call the main function
 if __name__ == "__main__":
