d76263fdae631cc2046cce1f9b888ed8476d3ef1
Melkxy2 <103466177+Melkxy2@users.noreply.github.com>
2024-03-22 16:16:02 -0400

    Update LED.py
    added error handling to LED

--- current pico files/LED.py
+++ current pico files/LED.py
@@ -1,35 +1,39 @@
-# March 14
 from machine import PWM, Pin
-
 class LED:
     def __init__(self, led_num):
-        if led_num == 1:
-            
-            self.wavelength = 1050
-            self.pwm_pin = PWM(Pin(12))
-            self.pwm_pin.freq(400)
-            self.pwm_pin.duty_u16(65535) 
-            self.adc_addr = 0
-            
-        elif led_num == 2:
-            self.wavelength = 1200
-            #self.pwm_pin = PWM(Pin(11))
-            self.pwm_pin = PWM(Pin(15))
-            self.pwm_pin.freq(400)
-            self.pwm_pin.duty_u16(65535)
-            self.adc_addr = 2
-            
-        elif led_num == 3:
-            self.wavelength = 1200
-            #self.pwm_pin = PWM(Pin(11))
-            self.pwm_pin = PWM(Pin(15))
-            self.pwm_pin.freq(400)
-            self.pwm_pin.duty_u16(65535)
-            self.adc_addr = 2
-            
-        elif led_num == 4:
-            self.wavelength = 1550
-            self.pwm_pin = PWM(Pin(13))
-            self.pwm_pin.freq(400)
-            self.pwm_pin.duty_u16(65535)
-            self.adc_addr = 1
+        try:
+            if led_num == 1:
+                
+                self.wavelength = 1050
+                self.pwm_pin = PWM(Pin(12))
+                self.pwm_pin.freq(400)
+                self.pwm_pin.duty_u16(65535) 
+                self.adc_addr = 1
+                
+            elif led_num == 2:
+                self.wavelength = 1200
+                #self.pwm_pin = PWM(Pin(11))
+                self.pwm_pin = PWM(Pin(15))
+                self.pwm_pin.freq(400)
+                self.pwm_pin.duty_u16(65535)
+                self.adc_addr = 2
+                
+            elif led_num == 3:
+                self.wavelength = 1200
+                #self.pwm_pin = PWM(Pin(11))
+                self.pwm_pin = PWM(Pin(15))
+                self.pwm_pin.freq(400)
+                self.pwm_pin.duty_u16(65535)
+                self.adc_addr = 2
+                
+            elif led_num == 4:
+                self.wavelength = 1550
+                self.pwm_pin = PWM(Pin(13))
+                self.pwm_pin.freq(400)
+                self.pwm_pin.duty_u16(65535)
+                self.adc_addr = 0
+        except Exception as e:
+            timestamp = "{:04d}-{:02d}-{:02d}_{:02d}-{:02d}-{:02d}".format(*current_time)
+            err_msg = f"Failed to initialize led, {e}, {timestamp}\n"
+            raise Exception(err_msg)
+
